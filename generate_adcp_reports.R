0b6f4c9482eb02d56eca2dd55f6c37e9e01d5b57
Joseph Barss <jospbarss@gmail.com>
2022-07-25 14:32:46 -0300

    Changes for compatibility with new package version

--- generate_adcp_reports.R
+++ generate_adcp_reports.R
@@ -12,24 +12,25 @@ report <- here("ADCP_Report.Rmd")
 tracker <- read_xlsx("Z:/Coastal Monitoring Program/ADCP/Side Lobe Trimmed/Reports/ADCP_Report_Tracker.xlsx")
 # tracker <- read_xlsx("Y:/Coastal Monitoring Program/ADCP/Side Lobe Trimmed/Reports/ADCP_Report_Tracker.xlsx")
 tracker <- tracker %>%
-  filter(is.na(`2022 Report (sidelobe trimmed)`))
+  slice(grep("No", `2022 Report (sidelobe trimmed)`, invert = TRUE))
 
 depl_date <- tracker$Depl_Date[]
 station <- tracker$Open_Data_Station[]
+county <- tracker$County[]
 
 
 # SECTION 2: Generate Reports --------------------------------------------------
 
-mapply(function(x, y) {
+mapply(function(x, y, z) {
 
   rmarkdown::render(
     input = report,
     output_file = paste0(
       x, "_", y, "_ADCP Report.docx"
     ),
-    params = list(depl_date = x, station = y),
+    params = list(depl_date = x, station = y, county = z),
   )
 
-}, depl_date, station)
+}, depl_date, station, county)
 
 
